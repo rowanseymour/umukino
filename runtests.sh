b38d9ff2ffd265ae5050da4bb7baa3292cb9aa0a
Rowan Seymour <rowanseymour@gmail.com>
2011-11-19 15:52:47 +0200

    Added first Google JS Test based unit test

---
+++ runtests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Runs unit tests using Google JS Test
+
+gjstest --js_files=\
+js/Vector2.js,\
+js/Host.js,\
+test/Vector2Test.js
---
+++ test/Vector2Test.js
@@ -0,0 +1,29 @@
+/**
+ * This file is part of Umukino
+ * 
+ * Umukino is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Umukino is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Umukino.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright Rowan Seymour 2011
+ */
+ 
+function Vector2Test() {}
+
+registerTestSuite(Vector2Test);
+
+Vector2Test.prototype.clone = function() {
+	var vec1 = new Vector2(1, 2);
+	var vec2 = vec2.clone();
+	expectEq(vec1.x, vec2.x);
+	expectEq(vec1.y, vec2.y);
+}
